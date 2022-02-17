637bb93cf45fd462c5cb84d8190ff2e50f4b2b23
DevKS-Yet <ksang0324@gmail.com>
2022-02-17 21:57:46 +0900

    20220217 - 프론트 컨트롤러 도입 - v1

--- src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -27,14 +27,19 @@ public class FrontControllerServletV1 extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        //어느 경로로 들어왔는지 값 저장
         String requestURI = request.getRequestURI();
 
+        //들어온 경로가 맵에 있는지 확인하고 있다면 변수 controller에 대입
         ControllerV1 controller = controllerMap.get(requestURI);
+
+        //만약 없는 URI를 입력했더라도 404가 아닌 500에러가 뜬다면 if문 안에 return을 적었는지 확인해보자
         if (controller ==  null) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
 
+        //변수 controller가 null이 아니라면 해당 컨트롤러의 프로세스 실행
         controller.process(request, response);
 
     }
