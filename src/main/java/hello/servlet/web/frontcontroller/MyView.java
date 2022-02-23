c13dfa3e493c563e95458a50baca73765faa0442
DevKS-Yet <ksang0324@gmail.com>
2022-02-23 23:56:41 +0900

    20220223 -Model 추가 v3

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
 
@@ -17,5 +18,17 @@ public class MyView {
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
+    }
+
+    //오버로드 꾸아아악~
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        modelToRequestAttribute(model, request); //model이 있는 값이 오면 메소드 실행
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        // 자바 8문법이며 model 이라는 Map 안에 담겨있는 key와 value를 포문으로 돌리며 request.setAttribute에 넣는 것
+        model.forEach((key, value) -> request.setAttribute(key, value));
     }
 }
